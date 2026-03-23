06bab0af0d065af66cbbb62904ebb6ad4a0b7184
dishagupta03 <guptadisha149@gmail.com>
2026-03-23 18:04:39 +0000

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,35 @@
+"use strict" //treat all JS code as newer version
+
+// alert(3+3) // we are using node js not browser , so this will give us error
+
+console.log(3+3);  console.log("Disha") // this is not readable , as ensure that readability should be high
+
+let name = "Disha"  //datatype is string
+let age = 18 // datatype is number that is int
+let isLoggedIn = false // datatype is boolean
+
+// Data Types
+
+// Number => 2 to power 53
+// bigInt 
+// String => ' ' , " "(prefer)                                                /*this all are primitive Data Types*/
+//boolean => true or false
+//null => it is a standalone value , representation of empty value
+//undefined => when we declare a variable but we dont assign value
+//symbol => use for uniqueness
+
+//object
+
+
+
+/*when we want to know the type of variable*/
+console.log(typeof age);   //number
+console.log(typeof name);  //string
+console.log(typeof isLoggedIn);  //boolean
+
+console.log(typeof null); //object
+console.log(typeof undefined);  //undefined
+
+
+
+
